fdabd4a7e0b12c203ad7ef73f180fa990c7b792e
Kutai kristof <73740605+kriskut08@users.noreply.github.com>
2024-08-07 12:19:27 +0200

    Update record.py
    The program was recording 137.9 hz intead of Mhz. Now it is fixed.
    
    (Now it works :D)

--- record.py
+++ record.py
@@ -7,8 +7,8 @@ tarnsDur =        sys.argv[3] # transit duration
 dirOfTheProgram = sys.argv[4] # this explains itself...
 satid           = sys.argv[5]
 #os.system('satdump live {i["mode"]} {outdir} --source rtlsdr --samplerate 2.4e6 --frequency {i["frequency"]} --lna_agc --gain 37 --timeout {str(transit.duration())} --http_server 127.0.0.1:8998')
-print(f'satdump record {outdir}/recording --source rtlsdr --samplerate 250e3 --frequency {freq} --gain 45 --baseband_format f32 --timeout {tarnsDur} --agc')
-os.system(f'satdump record {outdir}/recording --source rtlsdr --samplerate 250e3 --frequency {freq} --gain 45 --baseband_format f32 --timeout {tarnsDur} --agc')
+print(f'satdump record {outdir}/recording --source rtlsdr --samplerate 250e3 --frequency {freq}e6 --gain 45 --baseband_format f32 --timeout {tarnsDur} --agc')
+os.system(f'satdump record {outdir}/recording --source rtlsdr --samplerate 250e3 --frequency {freq}e6 --gain 45 --baseband_format f32 --timeout {tarnsDur} --agc')
 
 # the reason I'm separating the two stages, is because I'd like to have some way to re-decode 
 # a recrding if idk like meteor m2-4 broadcasts something where the rs check fails anyway this is 
